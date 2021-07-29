4af5e1104c7e652819f9d3c50447897c113a8b7b
Xavier Rochard <xrohard@gmail.com>
2021-07-30 00:14:52 +0200

    some dummy tests written

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,6 +1,17 @@
 from django.test import TestCase
 
+from app.calc import add, subtract
+
 
 class CalcTests(TestCase):
-    def test_dummy(self):
-        self.assertFalse(True)
+    def test_add_numbers(self):
+        self.assertEqual(add(3, 8), 11)
+
+    def test_add_return_correct_value(self):
+        self.assertNotEqual(add(3, 5), 11)
+
+    def test_subtract_two_numbers(self):
+        self.assertEqual(subtract(5, 11), 6)
+
+    def test_subtract_return_not_fixed_value(self):
+        self.assertNotEqual(subtract(5, 3), 6)
